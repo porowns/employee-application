6f200889f466614fef117c4e587ecdc09d79ced7
porowns <porowns@gmail.com>
2020-10-16 17:14:47 -0400

    Initial project commit, add model factory

---
+++ app/models/employee.js
@@ -0,0 +1,8 @@
+var createEmployee = (firstName, lastName, hireDate, role) => {
+    return {
+        "firstName": firstName, 
+        "lastName": lastName,
+        "hireDate": hireDate, 
+        "role": role, 
+    }
+}
